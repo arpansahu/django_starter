ddc09a0c2c037faedd7425610a91f2af7b0cdfcc
arpansahu <arpanrocks95@gmail.com>
2024-06-06 19:35:11 +0530

    in debug mode updated urls.py to acccept media files

--- django_starter/urls.py
+++ django_starter/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.views.generic import RedirectView
-
+from django.conf import settings
 from account.views import (
     CustomPasswordResetView,
     LogoutView,
@@ -51,3 +51,6 @@ urlpatterns = [
          auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
          name='password_reset_complete'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
